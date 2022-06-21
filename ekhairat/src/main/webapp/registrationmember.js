66c04c7d7cbb4ebc05db5eba7f72f041014011df
syaki <syaki@KILA>
2022-06-22 06:27:11 +0800

    try

--- ekhairat/src/main/webapp/registrationmember.js
+++ ekhairat/src/main/webapp/registrationmember.js
@@ -1,5 +1,5 @@
-var check = function() {
-  if (document.getElementById('password').value ==
+function check() {
+    if (document.getElementById('password').value ==
     document.getElementById('confirm_password').value) {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'matching';
@@ -7,7 +7,6 @@ var check = function() {
     document.getElementById('message').style.color = 'red';
     document.getElementById('message').innerHTML = 'not matching';
   }
-}
 
 
 function passwordtoogle() {
