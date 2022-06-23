84d1e6216a132e7be5393a88e19ac14b321aebd7
Akiemms <Akiemms@LAPTOP-3609MMP5>
2022-06-24 03:25:39 +0800

    Testing connect with DB v4

---
+++ ekhairat/src/main/java/ekhairat/Khairatmember.java
@@ -0,0 +1,47 @@
+package ekhairat;
+
+public class Khairatmember {
+	String memberID;
+	String memberName;
+	String memberContactNo;
+	String memberEmail;
+	String memberPassword;
+	String memberAddress;
+	
+	public String getmemberID() {
+		return memberID;
+	}
+	public void setmemberID(String memberID) {
+		this.memberID = memberID;
+	}
+	public String getmemberName() {
+		return memberName;
+	}
+	public void setmemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getmemberContactNo() {
+		return memberContactNo;
+	}
+	public void setmemberContactNo(String memberContactNo) {
+		this.memberContactNo = memberContactNo;
+	}
+	public String getmemberEmail() {
+		return memberEmail;
+	}
+	public void setmemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getmemberPassword() {
+		return memberPassword;
+	}
+	public void setmemberPassword(String memberPassword) {
+		this.memberPassword = memberPassword;
+	}
+	public String getmemberAddress() {
+		return memberAddress;
+	}
+	public void setmemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+}
