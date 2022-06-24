2735ee03e8efc29929e5b7c5a3ed74bfde16a014
Akiemms <Akiemms@LAPTOP-3609MMP5>
2022-06-24 12:28:32 +0800

    Testing db connection V5

---
+++ ekhairat/src/main/java/ekhairat/Khairatmember.java
@@ -0,0 +1,40 @@
+package ekhairat;
+
+public class Khairatmember {
+	String memberName;
+	int memberContactNo;
+	String memberEmail;
+	String memberPassword;
+	String memberAddress;
+	
+	public String getmemberName() {
+		return memberName;
+	}
+	public void setmemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public int getmemberContactNo() {
+		return memberContactNo;
+	}
+	public void setmemberContactNo(int memberContactNo) {
+		this.memberContactNo = memberContactNo;
+	}
+	public String getmemberEmail() {
+		return memberEmail;
+	}
+	public void setmemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getmemberPassword() {
+		return memberPassword;
+	}
+	public void setmemberPassword(String memberPassword) {
+		this.memberPassword = memberPassword;
+	}
+	public String getmemberAddress() {
+		return memberAddress;
+	}
+	public void setmemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+}
---
+++ ekhairat/src/main/java/ekhairat/KhairatmemberHandler.java
@@ -0,0 +1,110 @@
+package ekhairat;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class KhairatmemberHandler
+ */
+@WebServlet("/KhairatmemberHandler")
+public class KhairatmemberHandler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public registrationDao rd;
+	public void init(){
+		rd = new registrationDao();
+	}
+    /**
+     * Default constructor. 
+     */
+    public KhairatmemberHandler() {
+    	super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		
+		String action = request.getParameter("action");
+		try {
+			switch (action) {
+			case "createKhairatmember": 
+				createKhairatmember(request,response);
+			break;
+			/*case "deleteKhairatmember":
+				deleteKhairatmember(request,response;)
+			break;
+			case "updateKahiratmember":
+				updateKhairatmember(request,responses);
+				break;
+				case "cancel":
+				 * cancel(request,response);
+				 * break;*/
+			
+		}
+		}
+		catch (SQLException e) {
+			throw new ServletException(e);
+			}
+	}
+	
+	private void createKhairatmember(HttpServletRequest request, HttpServletResponse response)
+	throws SQLException, IOException{
+		
+		String memberName = request.getParameter("memberName");
+		int memberContactNo = Integer.parseInt(request.getParameter("memberContactNo"));
+		String memberAddress = request.getParameter("memberAddress");
+		String memberEmail = request.getParameter("memberEmail");
+		String memberPassword = request.getParameter("memberPassword");
+		
+		Khairatmember mem = new Khairatmember();
+		
+		mem.setmemberName(memberName);
+		mem.setmemberContactNo(memberContactNo);
+		mem.setmemberAddress(memberAddress);
+		mem.setmemberEmail(memberEmail);
+		mem.setmemberPassword(memberPassword);
+		
+		rd.createKhairatmember(mem);
+		response.sendRedirect("registrationmember.jsp");
+	}
+}
+	/*private void deleteKhairatmember(HttpServletRequest request, HttpServletResponse response)
+	throws SQLException, IOException{
+		int id = Integer.parseInt(request.getParameter("id"));
+		String memberName = request.getParameter("memberName");
+		int memberContactNo = Integer.parseInt(request.getParameter("memberContactNo"));
+		String memberAddress = request.getParameter("memberAddress");
+		String memberEmail = request.getParameter("memberEmail");
+		String memberPassword = request.getParameter("memberPassword");
+		
+		Member mem = new Member();
+		mem.setMemberID(id);
+		mem.setmemberName(memberName);
+		mem.setmemberContactNo(memberContactNo);
+		mem.setmemberAddress(memberAddress);
+		mem.setmemberPassword(memberPassword);
+		
+		rd.updateMember(mem);
+		
+		response.sendRedirect("registrationmember.jsp");
+		
+	}*/
