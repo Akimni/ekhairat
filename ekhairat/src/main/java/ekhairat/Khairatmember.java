bf6c7ae7f3bb3c750d675c02e4b85941ffd19c99
syaki <syaki@KILA>
2022-07-04 15:26:22 +0800

    update new

--- ekhairat/src/main/java/ekhairat/Khairatmember.java
+++ ekhairat/src/main/java/ekhairat/Khairatmember.java
@@ -2,48 +2,48 @@ package ekhairat;
 
 public class Khairatmember {
 	String memberName;
-	String memberID;
-	String memberContactNo;
-	String memberEmail;
-	String memberPassword;
-	String memberAddress;
-	
-	public Khairatmember() {}
-	
-	public String getmemberName() {
-		return memberName;
-	}
-	public void setmemberName(String memberName) {
-		this.memberName = memberName;
-	}
-	public String getmemberID() {
-		return memberID;
-	}
-	public void setmemberID(String memberID) {
-		this.memberID = memberID;
-	}
-	public String getmemberContactNo() {
-		return memberContactNo;
-	}
-	public void setmemberContactNo(String memberContactNo) {
-		this.memberContactNo = memberContactNo;
-	}
-	public String getmemberEmail() {
-		return memberEmail;
-	}
-	public void setmemberEmail(String memberEmail) {
-		this.memberEmail = memberEmail;
-	}
-	public String getmemberPassword() {
-		return memberPassword;
-	}
-	public void setmemberPassword(String memberPassword) {
-		this.memberPassword = memberPassword;
-	}
-	public String getmemberAddress() {
-		return memberAddress;
-	}
-	public void setmemberAddress(String memberAddress) {
-		this.memberAddress = memberAddress;
-	}
+    String memberID;
+    String memberContactNo;
+    String memberEmail;
+    String memberPassword;
+    String memberAddress;
+
+    public Khairatmember() {}
+
+    public String getmemberName() {
+        return memberName;
+    }
+    public void setmemberName(String memberName) {
+        this.memberName = memberName;
+    }
+    public String getmemberID() {
+        return memberID;
+    }
+    public void setmemberID(String memberID) {
+        this.memberID = memberID;
+    }
+    public String getmemberContactNo() {
+        return memberContactNo;
+    }
+    public void setmemberContactNo(String memberContactNo) {
+        this.memberContactNo = memberContactNo;
+    }
+    public String getmemberEmail() {
+        return memberEmail;
+    }
+    public void setmemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+    public String getmemberPassword() {
+        return memberPassword;
+    }
+    public void setmemberPassword(String memberPassword) {
+        this.memberPassword = memberPassword;
+    }
+    public String getmemberAddress() {
+        return memberAddress;
+    }
+    public void setmemberAddress(String memberAddress) {
+        this.memberAddress = memberAddress;
+    }
 }
